a3ebd3056eb92e8ae42adbb482c948fcc31afc2f
Gin Miraing <ginmiraing@outlook.com>
2024-05-29 19:25:00 +0800

    feat: add tailwind css

--- app/root.tsx
+++ app/root.tsx
@@ -6,12 +6,21 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import styles from "./tailwind.css?url";
+
+export const links = () => {
+  return [{ rel: "stylesheet", href: styles }];
+};
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
         <Meta />
         <Links />
       </head>
