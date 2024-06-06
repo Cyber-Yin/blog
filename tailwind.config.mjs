5e1110b56b2fc182a3131cf02d0ea41cb1f5bedd
Gin Miraing <ginmiraing@outlook.com>
2024-06-06 15:35:02 +0800

    feat: change style

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,38 @@
 export default {
   content: ["./app/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        theme: "var(--theme)",
+      },
+      borderColor: {
+        primary: "var(--border-primary)",
+        secondary: "var(--border-secondary)",
+      },
+      backgroundColor: {
+        primary: "var(--background-primary)",
+        secondary: "var(--background-secondary)",
+        comment: "var(--background-comment)",
+      },
+      textColor: {
+        primary: "var(--font-primary)",
+        secondary: "var(--font-secondary)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
